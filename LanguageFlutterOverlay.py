ab10a416ccf05ec9fc72f6add98ab04196d236ad
“renyushuang” <renyushuang501@163.com>
2020-03-17 16:54:27 +0800

    change: flutter语言新增，增加文件或文件夹的双重处理

--- LanguageFlutterOverlay.py
+++ LanguageFlutterOverlay.py
@@ -10,13 +10,13 @@
 import os, logging, json, sys
 from json import JSONDecodeError
 
-transDir = "./多语文件夹"
+transDir = "/Users/renyushuang/Downloads/aaaaaaaa"
 file_name = "多语"
 flutter_dir = "/Users/renyushuang/custom/projectDo/FlutterPDF/flutter_pdf/lib/res/global/l10n"
 
 
 def copyDataTiFlutter(jsonData, flutterFilePath):
-    flutterFile = open(flutterFilePath,"r")
+    flutterFile = open(flutterFilePath, "r")
     try:
         flutterJson = json.loads(flutterFile.read())
         flutterJson.update(jsonData)
@@ -25,32 +25,51 @@ def copyDataTiFlutter(jsonData, flutterFilePath):
         flutterFileW.write(json.dumps(flutterJson, indent=4, ensure_ascii=False))
         flutterFileW.close()
     except:
-        print("这个flutter文件有问题"+flutterFilePath)
+        print("这个flutter文件有问题" + flutterFilePath)
 
-for languageDir in os.listdir(transDir):
-    languageCode = languageDir.split("-")[0]
-    flutterFile = os.path.join(flutter_dir, "intl_" + languageCode + '.arb')
-    if os.path.exists(flutterFile):
-        for home, dirs, files in os.walk(os.path.join(transDir, languageDir)):
-            for file in files:
-                if file.find(file_name) != -1:
+
+def readDirFileData(languageDir):
+    for home, dirs, files in os.walk(os.path.join(transDir, languageDir)):
+        for file in files:
+            if file.find(file_name) != -1:
+                try:
+                    t = open(os.path.join(home, file))
+                    jsonData = json.loads(t.read().replace("”", "\""))
+                    t.close()
+                    copyDataTiFlutter(jsonData, flutterFile)
+                except JSONDecodeError:
                     try:
                         t = open(os.path.join(home, file))
-                        jsonData = json.loads(t.read().replace("”", "\""))
+                        jsonData = json.loads("{" + t.read().replace("”", "\"") + "}")
                         t.close()
                         copyDataTiFlutter(jsonData, flutterFile)
                     except JSONDecodeError:
-                        try:
-                            t = open(os.path.join(home, file))
-                            jsonData = json.loads("{" + t.read().replace("”", "\"") + "}")
-                            t.close()
-                            copyDataTiFlutter(jsonData, flutterFile)
-                        except JSONDecodeError:
-                            print("这个文件下的json有问题" + os.path.join(home, file))
-                    except:
-                        print("这个文件有问题" + os.path.join(home, file))
+                        print("这个文件下的json有问题" + os.path.join(home, file))
+                except:
+                    print("这个文件有问题" + os.path.join(home, file))
 
-            print("------------")
-        pass
+        print("------------")
+    pass
+
+
+for languageDir in os.listdir(transDir):
+    languageCode = None
+
+    if os.path.isdir(os.path.join(transDir, languageDir)):
+        # 文件夹
+        languageCode = languageDir.split("-")[0]
+    elif os.path.isfile(os.path.join(transDir, languageDir)):
+        # 文件
+        name = os.path.splitext(languageDir)[0]
+        # 文件以文件名字名字进行区分
+        languageCode = name.split("-")[1]
+        # 文件的文件夹就是翻译的文件
+        languageDir = transDir
+        print("filename = " + str(languageCode))
+
+    # flutter file 路径
+    flutterFile = os.path.join(flutter_dir, "intl_" + languageCode + '.arb')
+    if os.path.exists(flutterFile):
+        readDirFileData(languageDir)
     else:
         print("不存在该文件路径" + flutterFile)
